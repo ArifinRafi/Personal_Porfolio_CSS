4387624070efe3961b497671b17d3e0d0abb90a4
arifin rafi <ahmedrafi364@gmail.com>
2023-02-19 11:26:43 +0600

    footer added

--- js/home.js
+++ js/home.js
@@ -15,3 +15,21 @@ document.getElementById('projects').addEventListener('click', function()
     window.location.href = "projects.html";
 }
 )
+
+document.getElementById('facebook').addEventListener('click', function()
+{
+    window.location.href = "https://www.facebook.com/arifin.rafi.16/";
+}
+)
+
+document.getElementById('github').addEventListener('click', function()
+{
+    window.location.href = "https://github.com/ArifinRafi";
+}
+)
+
+document.getElementById('linkedin').addEventListener('click', function()
+{
+    window.location.href = "https://www.linkedin.com/in/md-arifin-ahmed-rafi-4490a1193/";
+}
+)

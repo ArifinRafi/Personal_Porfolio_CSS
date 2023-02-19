795d2f17d38b0c1146efa60470d9c9d9db1e724e
arifin rafi <ahmedrafi364@gmail.com>
2023-02-19 11:03:08 +0600

    js added

---
+++ js/home.js
@@ -0,0 +1,17 @@
+document.getElementById('home').addEventListener('click', function()
+{
+    window.location.href = "index.html";
+}
+)
+
+document.getElementById('about').addEventListener('click', function()
+{
+    window.location.href = "skills.html";
+}
+)
+
+document.getElementById('projects').addEventListener('click', function()
+{
+    window.location.href = "projects.html";
+}
+)
